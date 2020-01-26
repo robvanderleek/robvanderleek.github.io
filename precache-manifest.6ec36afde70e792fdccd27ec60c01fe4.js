ea1b2198da32f545e0b4363a2efa98519b49e3f4
Rob van der Leek <robvanderleek@gmail.com>
2020-01-26 20:24:29 +0100

    Updates

---
+++ precache-manifest.6ec36afde70e792fdccd27ec60c01fe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50eeb71ab9abca3daa6b83e5f0950c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6bfc06dc99cd37022a4",
+    "url": "/static/css/main.fc1aee13.chunk.css"
+  },
+  {
+    "revision": "fd5e5d0e95a5cbe18ee5",
+    "url": "/static/js/2.3fb0a7c8.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.3fb0a7c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6bfc06dc99cd37022a4",
+    "url": "/static/js/main.0e4eec2d.chunk.js"
+  },
+  {
+    "revision": "e986b59289686613a58a",
+    "url": "/static/js/runtime-main.8a668db7.js"
+  },
+  {
+    "revision": "a8b63dbcd835b111e404ecaa0f71f1a4",
+    "url": "/static/media/me-445x459.a8b63dbc.png"
+  }
+]);
