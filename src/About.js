d2f0d97d92cc29baf638608011b02b2cc3962a19
Rob van der Leek <robvanderleek@gmail.com>
2020-03-23 23:08:33 +0100

    Add blog tab entry

--- src/About.js
+++ src/About.js
@@ -10,17 +10,17 @@ export default function About() {
                 <h2>Rob van der Leek</h2>
                 <p><a href="mailto:robvanderleek@gmail.com">robvanderleek@gmail.com</a></p>
                 <span className="icons">
-                        <a href="https://twitter.com/robvanderleek"><i className="fab fa-twitter"/></a>
-                        <a href="https://www.linkedin.com/in/rob-van-der-leek-66596962"><i className="fab fa-linkedin"/></a>
-                        <a href="https://medium.com/@robvanderleek"><i className="fab fa-medium"/></a>
-                        <a href="https://github.com/robvanderleek"><i className="fab fa-github"/></a>
-                    </span>
+                    <a href="https://twitter.com/robvanderleek"><i className="fab fa-twitter"/></a>
+                    <a href="https://www.linkedin.com/in/rob-van-der-leek-66596962"><i className="fab fa-linkedin"/></a>
+                    <a href="https://medium.com/@robvanderleek"><i className="fab fa-medium"/></a>
+                    <a href="https://github.com/robvanderleek"><i className="fab fa-github"/></a>
+                </span>
             </Contact>
             <Info>
                 <p>Software engineer. Lives and works in The Netherlands. Studied Computer Science at Delft
-                    University of Technology. Currently working at a <a href="https://keplervision.eu">Computer
-                        Vision Machine Learning startup in Amsterdam</a>. Passion for software quality and software
-                    minimalism.
+                    University of Technology. Currently working at a <a href="https://keplervision.eu" target="_blank"
+                                                                        rel="noopener noreferrer">Computer Vision
+                        Machine Learning startup in Amsterdam</a>. Passion for software quality and software minimalism.
                     I've done some of my best development work under the shower.</p>
             </Info>
         </React.Fragment>
--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ function App() {
                 <Tabs value={value} onChange={handleChange} centered>
                     <Tab label="About" to="/" component={Link} disabled={value === 0}/>
                     <Tab label="Portfolio" to="/portfolio" component={Link} disabled={value === 1}/>
+                    <a className="tab"
+                       href="https://www.notion.so/robvanderleek/To-Whom-It-May-Concern-9abe523d95d9479b97c0d29ad1b7e0a1"
+                       target="_blank" rel="noopener noreferrer"><Tab label="Blog"/></a>
                 </Tabs>
             </AppBar>
             <Content>
