c35ac584c43d24e0a54950b5b98d0797bc1df218
Rob van der Leek <robvanderleek@gmail.com>
2021-08-03 06:02:32 +0200

    Fix dependabot auto-merge

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
+import {MemoryRouter} from "react-router-dom";
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    const {getByText} = render(
+        <MemoryRouter>
+            <App/>
+        </MemoryRouter>
+    );
+    expect(getByText(/About/i)).toBeInTheDocument();
 });
