6258c4fc8b4df4ea61787a70406043cf02ae7121
Rob van der Leek <robvanderleek@gmail.com>
2023-07-08 13:39:28 +0200

    Add buymeacoffee link

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -14,7 +14,8 @@ export default function About() {
                 <p><a href="mailto:robvanderleek@gmail.com">robvanderleek@gmail.com</a></p>
                 <span className="icons">
                     <Hyperlink href="https://twitter.com/robvanderleek"><i className="fab fa-twitter"/></Hyperlink>
-                    <Hyperlink href="https://mastodon.social/@robvanderleek"><i className="fab fa-mastodon"/></Hyperlink>
+                    <Hyperlink href="https://mastodon.social/@robvanderleek"><i
+                        className="fab fa-mastodon"/></Hyperlink>
                     <Hyperlink href="https://www.linkedin.com/in/rob-van-der-leek-66596962"><i
                         className="fab fa-linkedin"/></Hyperlink>
                     <Hyperlink href="https://medium.com/@robvanderleek"><i className="fab fa-medium"/></Hyperlink>
@@ -25,6 +26,8 @@ export default function About() {
                         className="fab fa-instagram"/></Hyperlink>
                     <Hyperlink href="https://unsplash.com/@robvanderleek"><i
                         className="fab fa-unsplash"/></Hyperlink>
+                    <Hyperlink href="https://www.buymeacoffee.com/robvanderleek"><i
+                        className="fas fa-mug-hot"/></Hyperlink>
                 </span>
             </Contact>
             <Info>
