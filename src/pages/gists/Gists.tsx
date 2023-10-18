f33fd2538c25000d90247f809670446e29e48f76
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-10-18 09:36:49 +0200

    Add loading indicator

--- src/pages/gists/Gists.tsx
+++ src/pages/gists/Gists.tsx
@@ -4,6 +4,7 @@ import {Gist} from "../../entities/Gist";
 import {Avatar, List, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
 import {Subject} from "@mui/icons-material";
 import {ItemList} from "../pages.style";
+import {BeatLoader, HashLoader} from "react-spinners";
 
 function isMarkDownFileObject(fo: any) {
     return fo.language && fo.language === 'Markdown';
@@ -34,6 +35,7 @@ function gistEntry(gist: Gist) {
 }
 
 export default function Gists() {
+    const [loading, setLoading] = useState(true);
     const [gists, setGists] = useState<Array<Gist>>([]);
 
     useEffect(() => {
@@ -43,18 +45,36 @@ export default function Gists() {
             const gistsWithMarkdownFiles = filterMarkdownGists(json);
             gistsWithMarkdownFiles.sort((a, b) => b.updated_at.localeCompare(a.updated_at));
             setGists(gistsWithMarkdownFiles);
+            setLoading(false);
         }
 
         loadGists();
     }, []);
 
-    return (
-        <ItemList>
-            <List>
-                {gists && gists.map((g, i) => <a key={i} target="_blank"
-                                                 rel="noopener noreferrer"
-                                                 href={`https://gist.io/@robvanderleek/${g.id}`}>{gistEntry(g)}</a>)}
-            </List>
-        </ItemList>
-    );
+    const renderLoader = () => {
+        return (
+            <ItemList>
+                <BeatLoader color="#FF3355"/>
+            </ItemList>
+        );
+    }
+
+    const renderGists = () => {
+        return (
+            <ItemList>
+                <List>
+                    {gists && gists.map((g, i) => <a key={i} target="_blank"
+                                                     rel="noopener noreferrer"
+                                                     href={`https://gist.io/@robvanderleek/${g.id}`}>{gistEntry(g)}</a>)}
+                </List>
+            </ItemList>
+        );
+    }
+
+    if (loading) {
+        return renderLoader();
+    } else {
+        return renderGists();
+    }
+
 }
