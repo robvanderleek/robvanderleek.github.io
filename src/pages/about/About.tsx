1d0087b2f28b2ff5cdd0633724c13db2ec4c2c86
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2025-04-16 18:01:37 +0200

    build: ⬆️ Fix thesis link

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -42,7 +42,7 @@ export default function About() {
             <Info>
                 <p><Hyperlink href="https://registry.jsonresume.org/robvanderleek">Lives and works in The
                     Netherlands.</Hyperlink></p>
-                <p><Hyperlink href="https://www.dropbox.com/s/4cmayfumnvzlzml/thesis.pdf?dl=0">Studied Computer Science
+                <p><Hyperlink href="https://drive.google.com/file/d/1xNJMVcWZTlptNlHN90OSFpjmsFquYJBP/view?usp=sharing">Studied Computer Science
                     at Delft University of Technology.</Hyperlink></p>
                 <p><Hyperlink href="https://keplervision.eu">Currently working at a Computer
                     Vision Machine Learning startup in Amsterdam.</Hyperlink></p>
