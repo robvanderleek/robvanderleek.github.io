9ce66ffd4e1990c01c56168f25ca2b8ce8e4a5aa
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2026-04-05 14:42:03 +0200

    ci: 💚 Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), svgrPlugin()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 3000,
     open: true
